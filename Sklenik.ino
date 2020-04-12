f2f3ece5bf73936a558f542e274e8b4638d0e80b
Valkys6 <63265244+Valkys6@users.noreply.github.com>
2020-04-13 00:01:58 +0200

    Update Sklenik.ino

--- Sklenik.ino
+++ Sklenik.ino
@@ -1,68 +1,85 @@
-/* Cerpadlo:
- *  Po přijmu zprávy "Relay_ON!" sepne relé na čerpadle
- *  Po příjmu zprávy "Relay_OF!" čerpadlo vypne
+/* Skleník:
+ *  Vyšli zprávu každé 2 sekundy
+ *  "Relay_ON!"při podmínce tlačítko + horní plovák v pozici HIGH
+ *  "Relay_ON!" při podmínce spodní plovák v pozici LOW
+ *  "Relay_OF!" pokud není některá z podmínek splněna
  *  Po příjmu jakékoli zprávy rozsvítí integrovanou LED
- *  V případě, že nepřichází zpráva, vypni relé po 10 sekundách
-*/
+ */
 
 // Použíté knihovny:
-#include <RH_ASK.h>
-#include <SPI.h>                 // Not actually used but needed to compile
+#include <RH_ASK.h>               //Knihovna ovladace radia
+#include <SPI.h>                  //Not actually used but needed to compile
+#include <RTClib.h>               //Knihovna ovaldace RTC (V originalu je teda "RTClib.h", ale snad to nebude vadit)
 
-#define  PIN_RELAY 2             //kde mame relatko
-RH_ASK driver;                   //objekt ovladace radia
-uint8_t reltim;                  //casovadlo, pozor, umi to max 255 vterin (max 0xFF)
+#define PIN_BUTTON1  2            //Pozice pro tlacitko 1 - zapni relé
+#define PIN_PLOV1  3              //Pozice pro horni plovakova senzor
+#define PIN_PLOV2  4              //Pozice pro dolni plovakova senzor
 
-void relay(uint8_t mode) {       //Funkce pro odeslani retezce
-  if (mode==0) {                 //prejem si vypnout
+// variables will change:
+int buttonState = 0;         // variable for reading the pushbutton status
+int plov1State = 0;            // proměnná pro výchozí stav horního plovákového senzoru
+int plov2State = 0;            // proměnná pro výchozí stav spodního plovákového senzoru
+char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+
+RH_ASK driver;                    //Objekt ovladace radia
+RTC_DS3231 rtc;                   //Objekt ovladace RTC
+uint8_t reltim;                   //Casovadlo, pozor, umi to max 255 vterin (max 0xFF) - Bude zde použito?
+
+void relay(uint8_t mode) {        //Funkce pro odeslani retezce na seriovy port - kontrola co to vlastne odesilame
+  if (mode==0) {                  //Prejem si vypnout tak ukaž na seriovém portu "Relay OFF"
     Serial.println(F("Relay OFF"));
-    digitalWrite(PIN_RELAY, LOW); //tak vypinam relatko    
-  } else { //prejem si zapnout
+  } else {                        //Prejem si zapnout tak ukaž na seriovém portu "Relay ON"
     Serial.println(F("Relay ON"));
-    digitalWrite(PIN_RELAY, HIGH); //zapni relatko
   }
 }
 
-void setup() {
-  //pripravime si seriak, abychom tam mohli zvracet moudra
-  Serial.begin(9600);            //Debugging only
-  Serial.println(F("Valkys super RF Cerpadlo driver v1.0\n=================================\n"));
-  pinMode(LED_BUILTIN, OUTPUT);  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(PIN_RELAY, OUTPUT);    // inicializace digitálního pinu pro RELAY
-  relay(0);                      //vypni to
-  reltim=0;                      //necasujeme 
-  if (!driver.init()) Serial.println("Radio driver init failed"); // nastavení komunikace rádiového přijímače - pin 11 (určeno asi knihovnou)
+void setup()  {
+  //pripravime si seriak, abychom tam mohli zvracet moudra, které zvracíme po rádiu
+  Serial.begin(9600);             //Debugging only
+  Serial.println(F("Valkys super RF Sklenik driver v1.0\n=================================\n"));
+  pinMode(LED_BUILTIN, OUTPUT);   //Inicializace LED_BUILDIN jako výstup
+  pinMode(PIN_BUTTON1, INPUT);    //Inicilazace tlacitka jako vstup
+  pinMode(PIN_PLOV1, INPUT);      //Inicializace horniho plovakoveho senzoru - PIN_PLOV1
+  pinMode(PIN_PLOV2, INPUT);      //Inicializace spodniho plovakoveho senzoru - PIN_PLOV2
+  relay(0);                       //Vypni to
+  reltim=0;                       //Necasujeme 
+  if (!driver.init()) Serial.println("init failed"); // nastavení komunikace radioveho vysilace - pin 12 (urceno asi knihovnou)
 }
 
-void loop() {
-  
-  //ja programuju pekne v C, cize nejprve delklarace, pouziti az dale, a asi pouzivam jiny funkce nez normalni arduinisti, treba strcmp ;-)
-  uint8_t buf[20];               //bufik, radeji s rezervickou
-  uint8_t buflen;                //promenna
+void send_msg(const char* msg) {  //Funkce pro odeslani retezce
+  driver.send((uint8_t *)msg, strlen(msg)); //posli to
+  driver.waitPacketSent();        //Cekej, az to bude cely venku                             
+  delay(2000);                    //Flakej se 2s
+}
 
-  //obsluha radia
-  buflen=sizeof(buf);            //do promenny naperu velikost bufiku, to asi slouzi radio knihovne, aby nejela neka za roh
-  if (driver.recv(buf, &buflen)) { //pokud nam neco dorazilo po radiu, tak se tomu budeme venovat
-    digitalWrite(LED_BUILTIN, HIGH);   //rozsvitime ledku, bo prisla zprava
-    Serial.print(F("Message: ")); //ukazeme
-    Serial.println((char*)buf);   //co to vlastne dorazilo
-    if (strcmp("Relay_ON!",buf)==0) { //pokud je to prikaz pro zapnuti
-      relay(1);                   //zapneme relatko
-      reltim=10;                  //a nastavim casovadlo, aby to po case zdechlo
-    } else if (strcmp("Relay_OF!",buf)==0) { //pokud je to prikaz pro vypnuti
-      relay(0);                   //vypneme relatko
-      reltim=0;                   //a casovadlo muzem zarazit, ikdyz by to nevadilo, ale je to tak hezcejsi
-    } else {                      //je to nejaka kravina,
-      Serial.println(F("Unknown command")); //budeme drzkovat
+void loop() {                     //Hlavni smyce arduina
+
+  buttonState = digitalRead(PIN_BUTTON1);  //Cti status tlacitka 1
+  plov1State = digitalRead(PIN_PLOV1);     //Cti status horniho plovaku
+  plov2State = digitalRead(PIN_PLOV2);     //Cti status spodniho plovaku
+
+  if (buttonState == LOW) {       //Pokud je sepnuto tlacitko pod podminkou
+      do {                        //tak dělej tohle
+        //if (plov1State == HIGH)   //zkontroluj jestli sud není úplně plnej a pokud ne
+        digitalWrite(LED_BUILTIN, HIGH);   //Rozsvit integrovanou LED ze odesilam zpravu
+        send_msg("Relay_ON!");    //volam funkci odeslani retezce
+        relay(1);                 //ukaz zpravu na seriovym portu, ze chceme zapnout relatko (cerpadlo)
+        delay(50);                //Počkej 50ms
+      } while (plov1State == LOW); { //dokud není sud plnej
+        }
+  }
+  else if (plov2State == LOW) {   //Nebo když je spodní plovák sepnut(v obrácené pozici se závažím), a to znamena ze hladina je pod plovakem, vysli "Relay_ON!"
+      do {                        //tak dělej tohle
+        //if (plov1State == HIGH)   //zkontroluj jestli sud není úplně plnej a pokud ne
+        digitalWrite(LED_BUILTIN, HIGH);   //Rozsvit integrovanou LED ze odesilam zpravu
+        send_msg("Relay_ON!");    //volam funkci odeslani retezce
+        relay(1);                 //ukaz zpravu na seriovym portu, ze chceme zapnout relatko (cerpadlo)
+        delay(50);                //Počkej 50ms
+      } while (plov1State == LOW); { //dokud není sud plnej
+        }
+  } else {                        //Nebo ve vsech ostatnich pripadech vysli "Relay_OF!"
+    digitalWrite(LED_BUILTIN, HIGH);  //Rozsvit integrovanou LED ze odesilam zpravu
+    send_msg("Relay_OF!");        //Vysli "Relay_OF!"
+    relay(0);                     //ukaz zpravu na seriovym portu, ze chceme vypnout relatko (cerpadlo)
     }
-  }
-  //casovadlo
-  delay(1000);                    //a cekame vterinu, takze cela smyce (fukci loop vola "system" furt dokola jak debil) pojede 1x za vterinu, takze i casovadlo bude ve vterinach      
-  //potrebujeme to i zde?
-  //digitalWrite(LED_BUILTIN, LOW); //zhasneme ledku
-  //if (reltim!=0) {                //pokud casovadlo jede, budeme casovat
-  //  if ((--reltim)==0) {          //cukneme a pokud to prave dojelo, 
-  //    relay(0);                   //a vypnem relatko        
-    //}
-  //}    
-}
+ }
