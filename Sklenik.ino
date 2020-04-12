f2d50db35ed5969bb4b1051d07e90aadbf63ff3a
Valkys6 <63265244+Valkys6@users.noreply.github.com>
2020-04-12 21:18:38 +0200

    Update Sklenik.ino

--- Sklenik.ino
+++ Sklenik.ino
@@ -1,96 +1,82 @@
 /* Skleník:
- *  Vyšli zprávu "Relay_ON!" (tlačítko) a sepni čerpadlo po dobu 10 sekund
- *  Čerpaldlo čte zprávu každé 3 sekundy
- *  Pokud nenbylo tlašítko sepnuto během 5s intervalu, vypni relé = pošli ("Relay_OF!")
+ *  Vyšli zprávu každé 2 sekundy
+ *  "Relay_ON!"při podmínce tlačítko + horní plovák v pozici HIGH
+ *  "Relay_ON!" při podmínce spodní plovák v pozici LOW
+ *  "Relay_OF!" pokud není některá z podmínek splněna
+ *  Po příjmu jakékoli zprávy rozsvítí integrovanou LED
  */
 
 // Použíté knihovny:
 #include <RH_ASK.h>
-#include <SPI.h> // Not actually used but needed to compile
+#include <SPI.h>                  //Not actually used but needed to compile
 
-//pozice pro tlačítko
-const int buttonPin = 2;     // the number of the pushbutton pin
-//pozice pro horní plovákový senzor
-const int plov1Pin = 3;
-//pozice pro dolní plovákový senzor
-const int plov2Pin = 4;
-// pozice pro integrovanou LED:
-const int ledPin =  13;      // the number of the LED pin
-
+#define PIN_BUTTON1  2            //Pozice pro tlacitko 1 - zapni relé
+#define PIN_PLOV1  3              //Pozice pro horni plovakova senzor
+#define PIN_PLOV2  4              //Pozice pro dolni plovakova senzor
 // variables will change:
-int buttonState = 0;         // variable for reading the pushbutton status
-int plov1State = 0;            // proměnná pro výchozí stav horního plovákového senzoru
-int plov2State = 0;            // proměnná pro výchozí stav spodního plovákového senzoru
+int buttonState = 0;              // variable for reading the pushbutton status
+int plov1State = 0;               // proměnná pro výchozí stav horního plovákového senzoru
+int plov2State = 0;               // proměnná pro výchozí stav spodního plovákového senzoru
 
-RH_ASK driver;
-void setup()
-{
-  // initialize the LED pin as an output:
-    pinMode(ledPin, OUTPUT);
-  // initialize the pushbutton pin as an input:
-    pinMode(buttonPin, INPUT);
-  // inicializace horního plovákového senzoru - plov1Pin
-    pinMode(plov1Pin, INPUT);
-  // inicializace spodního plovákového senzoru - plov2Pin
-    pinMode(plov2Pin, INPUT);
-  // nastavení komunikace rádiového vysílače - pin 12 (určeno asi knihovnou)
-    Serial.begin(9600);    // Debugging only
-    if (!driver.init())
-         Serial.println("init failed");
-}
+RH_ASK driver;                    //Objekt ovladace radia
+uint8_t reltim;                   //Casovadlo, pozor, umi to max 255 vterin (max 0xFF) - Bude zde použito?
 
-//Funkce pro odeslani retezce
-void send_msg(const char* msg) {
-  driver.send((uint8_t *)msg, strlen(msg)); //posli to
-  driver.waitPacketSent();  //cekej, az to bude cely venku                             
-  delay(1000); //flakej se 1 s
-}
-
-//Hlavni smyce arduina
-void loop() {
-    // read the state of the pushbutton value:
-  buttonState = digitalRead(buttonPin);
-  // čti status horního plováku
-  plov1State = digitalRead(plov1Pin);
- // čti status spodního plováku
-  plov2State = digitalRead(plov2Pin);
-
-  // check if the pushbutton is pressed. If it is, the buttonState is LOW:
-  if (buttonState == LOW) {
-  // pokud je splněna podmínka přeskoč na:
-  if (plov1State == LOW) {
-    // turn LED on:
-    digitalWrite(ledPin, HIGH);
-    send_msg("Relay_BT!");  //volam funkci odeslani retezce
-    goto Relay_BT;
+void relay(uint8_t mode) {        //Funkce pro odeslani retezce na seriovy port - kontrola co to vlastne odesilame
+  if (mode==0) {                  //Prejem si vypnout tak ukaž na seriovém portu "Relay OFF"
+    Serial.println(F("Relay OFF"));
+  } else {                        //Prejem si zapnout tak ukaž na seriovém portu "Relay ON"
+    Serial.println(F("Relay ON"));
   }
- } 
-  else {
-  // pokud není splněna podmínka přeskoč na:
-  goto Relay_OFF;
 }
 
-// GoTo funkce pro odeslání "Relay_BT!" při stisknutí tlačítka - odešle 5 x zprávu "Relay_BT!" po 1s  
-Relay_BT:
-    digitalWrite(ledPin, HIGH);
-    send_msg("Relay_BT!");  //volam funkci odeslani retezce
-    delay(1000); // čekej 1000ms
-    send_msg("Relay_BT!");  //volam funkci odeslani retezce
-    delay(1000); // čekej 1000ms
-    send_msg("Relay_BT!");  //volam funkci odeslani retezce
-    delay(1000); // čekej 1000ms
-    send_msg("Relay_BT!");  //volam funkci odeslani retezce
-    delay(1000); // čekej 1000ms
-    send_msg("Relay_BT!");  //volam funkci odeslani retezce
-    delay(1000); // čekej 1000ms
-
-// GoTo funkce pro odeslání "Relay_ON!" při stavu plováku 2 na pozici HIGH (Nízká hladina)
-Relay_ON:
-    digitalWrite(ledPin, HIGH);
-    send_msg("Relay_ON!");  //volam funkci odeslani retezce
-    delay(10000);   // čekej 10 s do další akce
-// GoTo funkce pro odeslání "Relay_OF!"
-Relay_OFF:
-    digitalWrite(ledPin, LOW);
-    send_msg("Relay_OF!");  //volam funkci odeslani retezce
+void setup()  {
+  //pripravime si seriak, abychom tam mohli zvracet moudra, které zvracíme po rádiu
+  Serial.begin(9600);             //Debugging only
+  Serial.println(F("Valkys super RF Sklenik driver v1.0\n=================================\n"));
+  pinMode(LED_BUILTIN, OUTPUT);   //Inicializace LED_BUILDIN jako výstup
+  pinMode(PIN_BUTTON1, INPUT);    //Inicilazace tlacitka jako vstup
+  pinMode(PIN_PLOV1, INPUT);      //Inicializace horniho plovakoveho senzoru - PIN_PLOV1
+  pinMode(PIN_PLOV2, INPUT);      //Inicializace spodniho plovakoveho senzoru - PIN_PLOV2
+  relay(0);                       //Vypni to
+  reltim=0;                       //Necasujeme 
+  if (!driver.init()) Serial.println("init failed"); // nastavení komunikace radioveho vysilace - pin 12 (urceno asi knihovnou)
 }
+
+void send_msg(const char* msg) {  //Funkce pro odeslani retezce
+  driver.send((uint8_t *)msg, strlen(msg)); //posli to
+  driver.waitPacketSent();        //Cekej, az to bude cely venku                             
+  delay(2000);                    //Flakej se 2s
+}
+
+void loop() {                     //Hlavni smyce arduina
+
+  buttonState = digitalRead(PIN_BUTTON1);  //Cti status tlacitka 1
+  plov1State = digitalRead(PIN_PLOV1);     //Cti status horniho plovaku
+  plov2State = digitalRead(PIN_PLOV2);     //Cti status spodniho plovaku
+
+  if (buttonState == LOW) {       //Pokud je sepnuto tlacitko pod podminkou
+    if (plov1State == HIGH) {     //zkontroluj jestli sud není úplně plnej a pokud ne
+      do {                        //tak dělej tohle
+        digitalWrite(LED_BUILTIN, HIGH);   //Rozsvit integrovanou LED ze odesilam zpravu
+        send_msg("Relay_ON!");    //volam funkci odeslani retezce
+        relay(1);                 //ukaz zpravu na seriovym portu, ze chceme zapnout relatko (cerpadlo)
+      } while (plov1State == LOW); { //dokud není sud plnej
+        }
+    }
+  }
+  else if (plov2State == LOW) {   //Nebo když je spodní plovák sepnut(v obrácené pozici se závažím), a to znamena ze hladina je pod plovakem, vysli "Relay_ON!"
+    if (plov1State == HIGH) {     //zkontroluj jestli sud není úplně plnej a pokud ne
+      do {                        //tak dělej tohle
+        digitalWrite(LED_BUILTIN, HIGH);   //Rozsvit integrovanou LED ze odesilam zpravu
+        send_msg("Relay_ON!");    //volam funkci odeslani retezce
+        relay(1);                 //ukaz zpravu na seriovym portu, ze chceme zapnout relatko (cerpadlo)
+      } while (plov1State == LOW); { //dokud není sud plnej
+        }
+    }
+  } else {                        //Nebo ve vsech ostatnich pripadech vysli "Relay_OF!"
+    digitalWrite(LED_BUILTIN, HIGH);  //Rozsvit integrovanou LED ze odesilam zpravu
+    send_msg("Relay_OF!");        //Vysli "Relay_OF!"
+    relay(0);                     //ukaz zpravu na seriovym portu, ze chceme vypnout relatko (cerpadlo)
+     }
+ }
+ 
