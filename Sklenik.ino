a7544ecee11aaf9eed88aae5835b66d56f11be0d
Valkys6 <63265244+Valkys6@users.noreply.github.com>
2020-04-15 21:24:34 +0200

    Update Sklenik.ino

--- Sklenik.ino
+++ Sklenik.ino
@@ -1,9 +1,11 @@
 /* Skleník:
- *  Vyšli zprávu každé 2 sekundy
- *  "Relay_ON!" při podmínce tlačítko + horní plovák v pozici HIGH
- *  "Relay_ON!" při podmínce spodní plovák v pozici LOW
- *  "Relay_OF!" pokud není některá z podmínek splněna
- *  Po příjmu jakékoli zprávy rozsvítí integrovanou LED
+ *  Vysli zpravu kazde 2 sekundy
+ *  "Relay_ON!" pri podmince tlacitko + horni plovak v pozici HIGH a drž, to dokud není Horní plovák v pozici LOW
+ *  "Relay_ON!" pri podmince spodni plovak v pozici LOW + GMT cas je mezi 20:00 a 5:00 (-2 hodiny letniho casu) 
+ *  "Relay_OF!" pokud neni neni zadna z predchozich podminek splnena
+ *  Serial port ukaze co to odesilame a pomoci RTC modulu DS3231 ukaze cas a stav teploty v krabici ridici jednotky skleniku
+ *  Po vyslani jakekoli zprávy rozsviti integrovanou LED
+ *  
  */
 
 // Použíté knihovny:
@@ -26,10 +28,10 @@ RTC_DS3231 rtc;                   //Objekt ovladace RTC
 uint8_t reltim;                   //Casovadlo, pozor, umi to max 255 vterin (max 0xFF) - Bude zde použito?
 
 void relay(uint8_t mode) {        //Funkce pro odeslani retezce na seriovy port - kontrola co to vlastne odesilame
-  if (mode==0) {                  //Prejem si vypnout tak ukaž na seriovém portu "Relay OFF"
-    Serial.println(F("Relay OFF"));
-  } else {                        //Prejem si zapnout tak ukaž na seriovém portu "Relay ON"
-    Serial.println(F("Relay ON"));
+  if (mode==0) {                  //Prejem si vypnout 
+    Serial.println(F("Relay OFF"));  // tak ukaz na seriovem portu "Relay OFF"
+  } else {                        //Prejem si zapnout 
+    Serial.println(F("Relay ON"));  //tak ukaz na seriovem portu "Relay ON"
   }
 }
 
@@ -41,9 +43,13 @@ void setup()  {
   pinMode(PIN_BUTTON1, INPUT);    //Inicilazace tlacitka jako vstup
   pinMode(PIN_PLOV1, INPUT);      //Inicializace horniho plovakoveho senzoru - PIN_PLOV1
   pinMode(PIN_PLOV2, INPUT);      //Inicializace spodniho plovakoveho senzoru - PIN_PLOV2
-  relay(0);                       //Vypni to
-  reltim=0;                       //Necasujeme 
+  relay(0);                       //ve vychozim stavu vysilej "Relay_OF!"
+  reltim=0;                       //Necasujeme - bude pouzito? Asi ne 
   if (!driver.init()) Serial.println("init failed"); // nastavení komunikace radioveho vysilace - pin 12 (urceno asi knihovnou)
+  if (! rtc.begin()) {            //Nastaveni komunikace RTC modulu 
+    Serial.println("Couldn't find RTC");  //Bude drzkovat, kdyz nenajde RTC modul (baterie v modulu by mela vydrzet 8 let) 
+    while (1);                    //Kdyz je RTC modul ziv a zdrav, posila data (absenci slozenych zavorek a tu jednicku nechapu)
+  }
 }
 
 void send_msg(const char* msg) {  //Funkce pro odeslani retezce
@@ -53,33 +59,46 @@ void send_msg(const char* msg) {  //Funkce pro odeslani retezce
 }
 
 void loop() {                     //Hlavni smyce arduina
-
   buttonState = digitalRead(PIN_BUTTON1);  //Cti status tlacitka 1
   plov1State = digitalRead(PIN_PLOV1);     //Cti status horniho plovaku
   plov2State = digitalRead(PIN_PLOV2);     //Cti status spodniho plovaku
+  DateTime time = rtc.now();      //Optej se jaky mame cas
+  Serial.print(String("GMT:")+time.timestamp(DateTime::TIMESTAMP_FULL));  //Ukaz GMT cas
+  Serial.println();               //Ukonci radek
+  Serial.print("Hour: ");         //Ukaz retezec s popisem, ze chceme ukazat hodinu
+  Serial.print(time.hour(), DEC);  //Jakou mame hodinu
+  Serial.println();               //Ukonci radek
+  Serial.print("Temperature: ");  //Ukaz retezec s popisem, ze chceme ukazat teplotu
+  Serial.print(rtc.getTemperature());  //Jakou mame teplotu
+  Serial.println(" C");           //At ma pan Celsius radost a ukonci radek
 
   if (buttonState == LOW) {       //Pokud je sepnuto tlacitko pod podminkou
-      do {                        //tak dělej tohle
-        //if (plov1State == HIGH)   //zkontroluj jestli sud není úplně plnej a pokud ne
+      while (plov1State == HIGH){                        //tak dělej tohle
         digitalWrite(LED_BUILTIN, HIGH);   //Rozsvit integrovanou LED ze odesilam zpravu
         send_msg("Relay_ON!");    //volam funkci odeslani retezce
+        Serial.println();         //ukonci radek
         relay(1);                 //ukaz zpravu na seriovym portu, ze chceme zapnout relatko (cerpadlo)
-        delay(50);                //Počkej 50ms
-      } while (plov1State == LOW); { //dokud není sud plnej
-        }
+        delay(1000);              //Počkej 1000ms
+        return 0;
+      } 
   }
   else if (plov2State == LOW) {   //Nebo když je spodní plovák sepnut(v obrácené pozici se závažím), a to znamena ze hladina je pod plovakem, vysli "Relay_ON!"
-      do {                        //tak dělej tohle
-        //if (plov1State == HIGH)   //zkontroluj jestli sud není úplně plnej a pokud ne
+      while (plov1State == HIGH){                        //tak dělej tohle
+        if (Serial.print(time.hour(), DEC)<=20);  //pokud mame mene nez 20 hodinu GMT, spust to. Pokud je vic, tak opust funkci (nechceme rusit cerpadlem sousedy)
+        if (Serial.print(time.hour(), DEC)>=5);  //pokud mame vice nez 5 hodinu GMT, spust to. Pokud je vic, tak opust funkci (nechceme rusit cerpadlem sousedy) 
         digitalWrite(LED_BUILTIN, HIGH);   //Rozsvit integrovanou LED ze odesilam zpravu
         send_msg("Relay_ON!");    //volam funkci odeslani retezce
-        relay(1);                 //ukaz zpravu na seriovym portu, ze chceme zapnout relatko (cerpadlo)
-        delay(50);                //Počkej 50ms
-      } while (plov1State == LOW); { //dokud není sud plnej
-        }
+        Serial.println();         //Ukonci radek
+        relay(1);                 //Ukaz zpravu na seriovym portu, ze chceme zapnout relatko (cerpadlo)
+        delay(1000);                //Počkej 50ms
+        return 0;
+      } 
   } else {                        //Nebo ve vsech ostatnich pripadech vysli "Relay_OF!"
     digitalWrite(LED_BUILTIN, HIGH);  //Rozsvit integrovanou LED ze odesilam zpravu
     send_msg("Relay_OF!");        //Vysli "Relay_OF!"
-    relay(0);                     //ukaz zpravu na seriovym portu, ze chceme vypnout relatko (cerpadlo)
+    Serial.println();             //Ukonci radek
+    relay(0);                     //Ukaz zpravu na seriovym portu, ze chceme vypnout relatko (cerpadlo)
     }
+    
+  delay(5000);                    //Cekej 5s
  }
